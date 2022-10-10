76ee1e58dac6d5fbb08e2162c3326f5d166e1c17
AleksanderNisenbaum <nisenblvm1@gmail.com>
2022-10-10 16:44:16 +0300

    Улучшенная версия калькулятора
    В поле ввода происходит математика нескольких действий

--- script.js
+++ script.js
@@ -15,75 +15,102 @@ let minus = document.getElementById("minus");
 let multiple = document.getElementById("multiple");
 let divide = document.getElementById("divide");
 let del = document.getElementById("del");
-let resultInput = document.getElementById("result");
+let result = document.getElementById("result");
 
-let firstNumber = '';
-let secondNumber = '';
-let mathAction = '';
-let total = 0;
 
-let first = 0;
-let second = 0;
+input0.onclick = function (){createString('0')};
+input1.onclick = function (){createString('1')};
+input2.onclick = function (){createString('2')};
+input3.onclick = function (){createString('3')};
+input4.onclick = function (){createString('4')};
+input5.onclick = function (){createString('5')};
+input6.onclick = function (){createString('6')};
+input7.onclick = function (){createString('7')};
+input8.onclick = function (){createString('8')};
+input9.onclick = function (){createString('9')};
+sum.onclick = function (){createString('+')};
+minus.onclick = function (){createString('-')};
+multiple.onclick = function (){createString('x')};
+divide.onclick = function (){createString('/')};
+dot.onclick = function (){createString('.')};
 
-function func(valueButton){
-    let dot_check = '.';
-    if(mathAction === '') {
-        let count = firstNumber.split(dot_check).length - 1;
-        if(count === 1 && valueButton === '.'){valueButton = '';}
-        else{firstNumber += valueButton}
-        resultInput.value = (firstNumber + ' ' + mathAction + ' ' + secondNumber);
+res.onclick = function (){calc(result.value)};
+
+function createString(num) {
+    let flag1 = 0;
+    let flag2 = 0;
+
+    if((result.value.length === 0) &&
+        (num === '.' || num === '+' || num === '-' || num === '/' || num === 'x'))
+
+    {result.value = result.value
+        flag1 = 1;}
+
+    if(((result.value[result.value.length - 1] === '+') ||
+            (result.value[result.value.length - 1] === '.') ||
+            (result.value[result.value.length - 1] === '-') ||
+            (result.value[result.value.length - 1] === '/') ||
+            (result.value[result.value.length - 1] === 'x')) &&
+        (num === '.' || num === '+' || num === '-' || num === '/' || num === 'x'))
+    {result.value = result.value
+        flag2 = 1;}
+
+    if((flag1 != 1) && (flag2 != 1)){
+        result.value += num;
     }
-    else{
-        let count = secondNumber.split(dot_check).length - 1;
-        if(count === 1 && valueButton === '.'){valueButton = '';}
-        else{secondNumber += valueButton}
-        resultInput.value = (firstNumber + ' ' + mathAction + ' ' + secondNumber);
+    console.log(result.value);
+}
+
+function calc(resultValue){
+    let arrSymbols = [];
+    let str1 = '';
+    for (let i in resultValue){
+        if((resultValue[i] === '-')||
+            (resultValue[i] === '+')||
+            (resultValue[i] === 'x')||
+            (resultValue[i] === '/')){
+            arrSymbols.push(str1);
+            str1 = '';
+            str1 = resultValue[i];
+            arrSymbols.push(str1);
+            str1 = '';
+        }
+        else{
+            str1 += resultValue[i];
+        }
     }
-    console.log(firstNumber, mathAction, secondNumber);
+    arrSymbols.push(str1);
+    console.log(arrSymbols);
 
+    function math(arrSymbols, sign, i){
+        arrSymbols[i-2] = eval(Number(arrSymbols[i-2]) + sign + Number(arrSymbols[i]))
+        arrSymbols.splice(i-1, 2);
+        console.log(arrSymbols)
+    }
+
+    let j = 0
+    let arrLength = (arrSymbols.length-1)/2
+    while(j < arrLength){
+        console.log(j)
+        for (let i in arrSymbols){
+            if (arrSymbols[i-1] === '/'){math(arrSymbols, '/', i); break;};
+            if (arrSymbols[i-1] === 'x'){math(arrSymbols, '*', i); break;};
+        }
+        j++;
+    }
+
+    j = 0;
+    arrLength = (arrSymbols.length-1)/2
+    while(j < arrLength){
+        for (let i in arrSymbols){
+            if (arrSymbols[i-1] === '+'){math(arrSymbols, '+', i); break;};
+            if (arrSymbols[i-1] === '-'){math(arrSymbols, '-', i); break;};
+        }
+        j++;
+    }
+
+    result.value = arrSymbols;
 }
-input0.onclick = function (){func('0')};
-input1.onclick = function (){func('1')};
-input2.onclick = function (){func('2')};
-input3.onclick = function (){func('3')};
-input4.onclick = function (){func('4')};
-input5.onclick = function (){func('5')};
-input6.onclick = function (){func('6')};
-input7.onclick = function (){func('7')};
-input8.onclick = function (){func('8')};
-input9.onclick = function (){func('9')};
-dot.onclick = function (){func('.')};
-del.onclick = function (){
-    resultInput.value = '';
-    firstNumber = '';
-    secondNumber = '';
-    mathAction = '';
-};
 
+del.onclick = function (){result.value = ''};
 
-function funAction(act){
-    mathAction = act;
-    console.log(firstNumber, mathAction, secondNumber);
-    resultInput.value = (firstNumber + ' ' + mathAction + ' ' + secondNumber);
-}
-sum.onclick = function (){funAction('+')};
-minus.onclick = function (){funAction('-')};
-multiple.onclick = function (){funAction('X')};
-divide.onclick = function (){funAction('/')};
-
-
-res.onclick = function (){
-    first = Number(firstNumber);
-    second = Number(secondNumber);
-    if (mathAction === "+"){ total = first + second;}
-    if (mathAction === "-"){ total = first - second;}
-    if (mathAction === "X"){ total = first * second;}
-    if (mathAction === "/"){ total = first / second;}
-    firstNumber = String(total);
-    secondNumber = '';
-    mathAction = '';
-
-    // console.log(total, firstNumber, mathAction, secondNumber);
-
-    resultInput.value = total;
-}
