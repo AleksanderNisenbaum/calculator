b7325af0dde8817708c1c570d8b7c5fe0eece867
Nick Yakovenko <nikita.yakovenko@gmail.com>
2022-10-22 09:52:33 +0200

    my stack attempt

--- script.js
+++ script.js
@@ -1,119 +1,83 @@
-const res = document.getElementById("res");
-const dot = document.getElementById("dot");
-const input0 = document.getElementById("input0");
-const input1 = document.getElementById("input1");
-const input2 = document.getElementById("input2");
-const input3 = document.getElementById("input3");
-const input4 = document.getElementById("input4");
-const input5 = document.getElementById("input5");
-const input6 = document.getElementById("input6");
-const input7 = document.getElementById("input7");
-const input8 = document.getElementById("input8");
-const input9 = document.getElementById("input9");
-const sum = document.getElementById("sum");
-const minus = document.getElementById("minus");
-const multiple = document.getElementById("multiple");
-const divide = document.getElementById("divide");
-const del = document.getElementById("del");
 let result = document.getElementById("result");
-console.log(result);
+let calculate = document.getElementById("calculate");
 
-input0.onclick = () => modifyExpression('0');
-input1.onclick = () => modifyExpression('1');
-input2.onclick = () => modifyExpression('2');
-input3.onclick = () => modifyExpression('3');
-input4.onclick = () => modifyExpression('4');
-input5.onclick = () => modifyExpression('5');
-input6.onclick = () => modifyExpression('6');
-input7.onclick = () => modifyExpression('7');
-input8.onclick = () => modifyExpression('8');
-input9.onclick = () => modifyExpression('9');
-sum.onclick = () => modifyExpression('+');
-minus.onclick = () => modifyExpression('-');
-multiple.onclick = () => modifyExpression('x');
-divide.onclick = () => modifyExpression('/');
-dot.onclick = () => modifyExpression('.');
+let strOfNumbers = '';
+let arrOfStack = [];
+let symbol = '';
+let action = '';
+let resSumMinus = []
+function math(num){
 
-res.onclick = () => calc(result.value);
+    if((Math.sign(num)) === 1){
+        strOfNumbers+=num
+        console.log(strOfNumbers)
+        symbol = '';
+    }
+    else{
+        if((num === '.') && (strOfNumbers !== '') && (strOfNumbers[strOfNumbers.length-1] !== '.')){
+            strOfNumbers+=num
+            console.log(strOfNumbers)
+        }
+        if(num !== '.'){
+            if(strOfNumbers !== ''){arrOfStack.push(strOfNumbers)}
+            strOfNumbers = ''
 
-function modifyExpression(num) {
-    let stringStartIndicator = 0;
-    let indicatorRepetitionSigns = 0;
-
-    if((result.value.length === 0) &&
-        (num === '.' || num === '+' || num === '/' || num === 'x')){
-        result.value = result.value;
-        stringStartIndicator = 1;}
-
-    let lastSymbolString = result.value[result.value.length - 1];
-    if(((lastSymbolString === '+') ||
-            (lastSymbolString === '.') ||
-            (lastSymbolString === '-') ||
-            (lastSymbolString === '/') ||
-            (lastSymbolString === 'x')) &&
-        (num === '.' || num === '+' || num === '-' || num === '/' || num === 'x'))
-        {result.value = result.value;
-        indicatorRepetitionSigns = 1;}
-
-    if((stringStartIndicator === 0) && (indicatorRepetitionSigns === 0)){
-            if ((result.value.length) < 15){
-                result.value += num;
+            console.log(arrOfStack)
+            if(action === '/'){
+                arrOfStack[arrOfStack.length-3] = arrOfStack[arrOfStack.length-3] / arrOfStack[arrOfStack.length-1]
+                arrOfStack.splice((arrOfStack.length-2), 2)
+                action = ''
             }
+            if(action === 'x'){
+                arrOfStack[arrOfStack.length-3] = arrOfStack[arrOfStack.length-3] * arrOfStack[arrOfStack.length-1]
+                arrOfStack.splice((arrOfStack.length-2), 2)
+                action = ''
+            }
+
+            if((num === '-')&&(arrOfStack.length === 0)){strOfNumbers+=num}
+            if((symbol === '')&&(arrOfStack.length !== 0)){arrOfStack.push(num)}
+            symbol='sim'
+            if(num === '/'){action = num}
+            if(num === 'x'){action = num}
+            console.log(action)
         }
-    console.log(result.value);
+    }
+
+
+    if(Math.sign(arrOfStack[arrOfStack.length-1]) !== 1){
+        let arrValue = arrOfStack
+        result.value = arrValue.join('') + strOfNumbers
+    }
+    else{
+        let arrValue2 = arrOfStack
+        result.value = arrValue2.join()
+    }
+    console.log(arrOfStack)
+    console.log(strOfNumbers)
 }
 
-function calc(resultValue){
-    let arrSymbols = [];
-    let str1 = '';
-    for (let i in resultValue){
-        if((resultValue[i] === '-')||
-            (resultValue[i] === '+')||
-            (resultValue[i] === 'x')||
-            (resultValue[i] === '/')){
-            arrSymbols.push(str1);
-            str1 = '';
-            str1 = resultValue[i];
-            arrSymbols.push(str1);
-            str1 = '';
+function calc(array, string){
+
+    function sumMinus(array){
+        resSumMinus = Number(array[0])
+        for(let i = 0; i < array.length;i += 1 ){
+            if(array[i-1] === '+'){resSumMinus += Number(array[i])}
+            if(array[i-1] === '-'){resSumMinus -= Number(array[i])}
+            result.value = resSumMinus
+            arrOfStack = resSumMinus
+            console.log(arrOfStack)
         }
-        else{
-            str1 += resultValue[i];
-        }
+        console.log(array)
     }
-    arrSymbols.push(str1);
-    console.log(arrSymbols);
-
-
-    let j = 0
-    let arrLength = (arrSymbols.length-1)/2
-    while(j < arrLength){
-        console.log(j)
-        for (let i in arrSymbols){
-            if (arrSymbols[i-1] === '/'){arrSymbols[i-2] = Number(arrSymbols[i-2]) / Number(arrSymbols[i])
-                arrSymbols.splice(i-1, 2); break}
-
-            else if (arrSymbols[i-1] === 'x'){arrSymbols[i-2] = Number(arrSymbols[i-2]) * Number(arrSymbols[i])
-                arrSymbols.splice(i-1, 2); break}
-        }
-        j++;
+    if(strOfNumbers !== '') {math('+')
+        sumMinus(arrOfStack)
     }
-
-    j = 0;
-    arrLength = (arrSymbols.length-1)/2
-    while(j < arrLength){
-        for (let i in arrSymbols){
-            if (arrSymbols[i-1] === '+'){arrSymbols[i-2] = Number(arrSymbols[i-2]) + Number(arrSymbols[i])
-                arrSymbols.splice(i-1, 2); break}
-
-            else if (arrSymbols[i-1] === '-'){arrSymbols[i-2] = Number(arrSymbols[i-2]) - Number(arrSymbols[i])
-                arrSymbols.splice(i-1, 2); break}
-        }
-        j++;
+    else{
+        sumMinus(arrOfStack)
     }
-
-    result.value = arrSymbols;
 }
-
-del.onclick = function (){result.value = ''};
-
+const elements = document.querySelectorAll("div")
+elements.forEach(element => {
+    element.onclick = () => math(element.dataset.symbol)
+});
+calculate.onclick = function(){calc(arrOfStack, strOfNumbers)};
